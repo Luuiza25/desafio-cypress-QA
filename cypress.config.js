a00710950eeb0f4ce3144ca89d125b5a8333c949
Luuiza25 <89809445+Luuiza25@users.noreply.github.com>
2025-11-17 09:03:41 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,1 +1,10 @@
+const { defineConfig } = require('cypress')
 
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://lojaebac.ebaconline.art.br',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
+    setupNodeEvents(on, config) {}
+  }
+})
