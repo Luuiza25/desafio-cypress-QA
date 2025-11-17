e6a94bbdbf6ae10858c8a5866674c362814d3308
Luuiza25 <89809445+Luuiza25@users.noreply.github.com>
2025-11-17 09:11:21 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,1 +1,18 @@
-describe('Login', () => { it('login com sucesso', () => {}) });
+/// <reference types="cypress" />
+describe('Login - Fluxos', () => {
+  beforeEach(() => cy.visit('/'))
+
+  it('Login com credenciais válidas (se existir formulário)', () => {
+    cy.get('a, button').contains(/entrar|login|sign in/i).first().click({ force: true })
+    cy.get('input[type="email"], input[name="email"], input[id*="email"]').first().type('usuario@test.com')
+    cy.get('input[type="password"], input[name="password"], input[id*="password"]').first().type('senha123{enter}')
+    cy.contains(/meu perfil|sair|logout|bem-vindo/i, { timeout: 8000 }).should('exist')
+  })
+
+  it('Login com senha inválida deve mostrar erro', () => {
+    cy.get('a, button').contains(/entrar|login|sign in/i).first().click({ force: true })
+    cy.get('input[type="email"], input[name="email"], input[id*="email"]').first().type('usuario@test.com')
+    cy.get('input[type="password"], input[name="password"], input[id*="password"]').first().type('senhaErrada{enter}')
+    cy.contains(/senha incorreta|credenciais inválidas|error|invalid/i).should('exist')
+  })
+})
